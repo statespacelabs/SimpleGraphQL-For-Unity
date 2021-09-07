fdc773d48071b0f5e42b8a98fbf5a2e64b120f73
Jon Bowen <jonathanbowen@statespacelabs.com>
2021-09-07 12:03:22 -0400

    Using default json serializer for BrainLab to allow for Vector and color serialization.

--- Runtime/SimpleGraphQL/Request.cs
+++ Runtime/SimpleGraphQL/Request.cs
@@ -3,6 +3,7 @@ using System.Text;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using UnityEngine;
 
 namespace SimpleGraphQL
 {
@@ -23,6 +24,8 @@ namespace SimpleGraphQL
         }
     }
 
+
+
     [PublicAPI]
     public static class RequestExtensions
     {
@@ -31,14 +34,22 @@ namespace SimpleGraphQL
             return Encoding.UTF8.GetBytes(request.ToJson());
         }
 
-        public static string ToJson(this Request request,
-            bool prettyPrint = false)
+        public static string ToJson(this Request request, bool prettyPrint = false)
         {
-            return JsonConvert.SerializeObject
-            (   request,
-                prettyPrint ? Formatting.Indented : Formatting.None,
-                new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore}
-            );
+            // JsonConverter[] converters = new JsonConverter[] {
+            // new Vec2Conv(),
+            // new Vec2NullableConv(),
+            // new Vec3Conv(),
+            // new Vec3NullableConv(),
+            // new Vec4Conv(),
+            // new ColorNullableConv()
+            // };
+
+            // return JsonConvert.SerializeObject
+            // (request, prettyPrint ? Formatting.Indented : Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, Converters = converters });
+
+             return JsonConvert.SerializeObject
+            (request, prettyPrint ? Formatting.Indented : Formatting.None);
         }
     }
 }
